76dee7aa2a09ddb1a7395d9c5bd36da088041ce4
Ad2m1337 <167755532+Ad2m1337@users.noreply.github.com>
2024-05-13 21:11:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,31 +2,100 @@
 var main_div = document.querySelector(".main");
 var topbar_div = document.querySelector(".topbar");
 var button1 = document.querySelector(".button1");
+var table1 = document.querySelector(".table1");
 
-function notify_anim(status, text) {
+async function notify_anim(status, text) {
+    button1.disabled = true;
     if(status == "error") {
-       var element = document.createElement("div");
+        var element = document.createElement("div");
+        element.style.backgroundColor = "red";
+        element.setAttribute("class", "anim");
 
-       element.setAttribute("class", "anim");
-
-       element.innerHTML = `
+        element.innerHTML = `
             <div>Error</div>
             <div>${text}</div>
-       `
+        `
 
-       main_div.insertBefore(element, topbar_div);
+        main_div.insertBefore(element, topbar_div);
     }
 
     if(status == "success") {
+        var element = document.createElement("div");
+        element.style.backgroundColor = "rgb(7, 211, 7)";
+        element.setAttribute("class", "anim");
 
+        element.innerHTML = `
+            <div>Sucess</div>
+            <div>${text}</div>
+        `
+
+        main_div.insertBefore(element, topbar_div);
     }
 
-    return
+    const animationPromise = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, 4500);
+    });
+
+    animationPromise.then(() => {
+        element.remove(); 
+        button1.disabled = false;
+    });
 }
 
+function addCell(tekst, table_row) {
+    var row = document.createElement("td");
+    row.textContent = tekst;
+    table_row.append(row);
+
+    return table_row
+}
+
+function removeelem() {
+    this.remove();
+}
 
 button1.addEventListener("click", () => {
-    notify_anim("error", "Puste pole marka");
+    var inputs = document.querySelectorAll(".input_dane")
+    if (!button1.disabled) {
+        var inputarr = [...inputs];
+        var input_checked = inputarr.some(input => !input.value);
+        
+        if (!input_checked) {
+            var table_row = document.createElement("tr");
+
+            addCell(`${inputarr[0].value} ${inputarr[1].value}`, table_row)
+            addCell(`${inputarr[2].value}`, table_row)
+            addCell(`${inputarr[3].value}`, table_row)
+            addCell(`${inputarr[4].value}`, table_row)
+            addCell(`${inputarr[5].value}`, table_row)
+
+
+            var spalanie = (inputarr[7].value / inputarr[6].value) * 100;  
+            if(isNaN(spalanie)) {
+                spalanie = "-"
+                addCell(`-`, table_row)
+                alert("PUSTE POSLEK")
+            }
+            else {
+                addCell(`${Math.round(spalanie)}L`, table_row)
+            }
+             
+            table1.append(table_row)
+            table_row.addEventListener("click", removeelem)
+            notify_anim("success", "Dodano!")
+        }
+        else {
+            inputs.forEach(input => {
+                if(!input.value) {
+                    notify_anim("error", `Puste pole ${input.getAttribute("placeholder")}`)
+                }
+            });
+        }
+
+        
+    }
 })
 
 
